516bd0648ca9c37306e337b8ef1d9b0db2d60e37
fresh5447 <doug@kosmojo.com>
2015-11-07 15:19:15 -0700

    fix index styling

--- client/NavBar.js
+++ client/NavBar.js
@@ -3,7 +3,7 @@ var React = require('react');
 var NavBar = React.createClass({
 	render: function(){
 		return(
-		<nav className="navbar navbar-default">
+		<nav className="navbar navbar-default navbar-fixed-top">
             <div className="container-fluid">
                 <div className="navbar-header">
                     <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
